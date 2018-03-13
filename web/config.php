075d899031f8872b66d503fb0dec3f04b56e7268
hibest <37336313+hibest@users.noreply.github.com>
2018-03-13 23:59:05 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,7 +2,7 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyCxi7CDBoK8K_zEq35NPV_rAOPovdpEYhU');//这里改成您的api key
 define('GJ_CODE', 'US');//国家代码
-define('SITE_NAME', 'U2B');//网站名字
+define('SITE_NAME', 'Ericus Youtube');//网站名字
 define('TITLENAME', 'U2B');//网站名字
 define('EN2DEKEY', 'kyfAAcxhgv0{/W.>WtoPTt+rCO|g9i');//一个随机字符串，加解密KEY
 define('EMAIL', 'hibest@live.com');//邮箱
